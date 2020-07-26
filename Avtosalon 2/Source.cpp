b30b21632c04831d6ea2c657ed96f15276dd9499
Andrii <andrijprihodko@gmail.com>
2020-07-26 17:31:55 +0300

    add method - "add a car"

--- Avtosalon 2/Source.cpp
+++ Avtosalon 2/Source.cpp
@@ -9,8 +9,52 @@ struct Auto
     int year;
     float capacity;
     int price;
+	Auto* next = nullptr;
 };
 
+class AutoSalon
+{
+public:
+	Auto* AddAuto(Auto* Head, int Number) {
+		Number--;
+		Auto* NewItem = new(Auto);
+		cout << "Year of car production: ";
+		cin >> NewItem->year;
+		cout << "Name: ";
+		cin >> NewItem->name;
+		cout << "Price: ";
+		cin >> NewItem->price;
+		cout << "Engine capacity: ";
+		cin >> NewItem->capacity;
+		NewItem->next = nullptr;
+		if (Head == nullptr)
+		{
+			Head = NewItem;
+		}
+		else {
+			Auto* Current = Head;
+			for (int i = 1; i < Number && Current->next != nullptr; i++)
+			{
+				Current = Current->next;
+			}
+			if (Number == 0) {
+
+				NewItem->next = Head;
+				Head = NewItem;
+			}
+			else {
+				if (Current->next != nullptr)
+				{
+					NewItem->next = Current->next;
+				}
+
+				Current->next = NewItem;
+			}
+		}
+		return Head;
+	}
+
+};
 
 
 int main()
