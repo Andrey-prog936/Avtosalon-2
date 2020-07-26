b4aae1968c875ab7c977a3a65d6f4cc63b37c3fb
Andrii <andrijprihodko@gmail.com>
2020-07-26 17:27:08 +0300

    Create struct

---
+++ Avtosalon 2/Source.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct Auto
+{
+    string name;
+    int year;
+    float capacity;
+    int price;
+};
+
+
+
+int main()
+{
+
+    return 0;
+}
